cd58986cb70e88f7e0b9ccaa89f72df67e577be6
DiogoFerreira19 <diogoalexf@gmail.com>
2025-11-01 00:26:38 +0000

    wip

--- web_portfolio/src/components/NavBar/NavBar.jsx
+++ web_portfolio/src/components/NavBar/NavBar.jsx
@@ -6,10 +6,16 @@ function Navbar(){
         <nav>
             <h4 className='nav-brand'>Diogo Ferreira</h4>
             <div className="nav-options">
-                <a className='nav-link big-body'>Cenas</a>
-                <a className='nav-link big-body'>Cenas</a>
-                <a className='nav-link big-body'>Cenas</a>
+                <a className='nav-link big-body'>Home</a>
+                <a className='nav-link big-body'>Projects</a>
+                <a className='nav-link big-body'>Skills</a>
+                <a className='nav-link big-body'>About</a>
+                <div className='nav-language'>
+                    <p className='nav-language-option b'>PT</p>
+                    <p className='nav-language-option b'>EN</p>
+                </div>
             </div>
+
         </nav>
     );
 } 
