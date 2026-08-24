3887378cff39be9de19f8709f1640e887f1fd23e
DiogoFerreira19 <diogoalexf@gmail.com>
2026-08-24 21:05:24 +0100

    fix: domain

--- web_portfolio/src/App.js
+++ web_portfolio/src/App.js
@@ -1,14 +1,20 @@
-import {HomePage} from './pages'
-import './App.css';
-import { LanguageProvider, PageProvider } from './contexts';
+import { HomePage } from "./pages";
+import "./App.css";
+import { LanguageProvider, PageProvider } from "./contexts";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    if (window.location.hostname === "w1ntrypyt.github.io") {
+      window.location.replace("https://dferreira.pt");
+    }
+  }, []);
   return (
     <div className="App">
       <PageProvider>
-      <LanguageProvider>
-        <HomePage/> 
-      </LanguageProvider>
+        <LanguageProvider>
+          <HomePage />
+        </LanguageProvider>
       </PageProvider>
     </div>
   );
